9b8cb705825ea75c328a74873256fcef197092dd
Brais <braisfv22@gmail.com>
2023-03-21 20:00:40 +0100

    url endpoint

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"Hello": "World"}
+
+
+@app.get("/url")
+async def url():
+    return {"url": "https://github.com/braiso-22/python-backend"}
